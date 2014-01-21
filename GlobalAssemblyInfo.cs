06c6e7b5b607b7a8bac06177dd630def4d3b23b7
Hans Wolff <hans.wolff.home@gmail.com>
2014-01-21 22:16:15 +0100

    added NodeGreetingMessage and NodeType for future client/server connection validation

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // 
+
 using System.Reflection;
 
 [assembly: AssemblyCompany("Hans Wolff")]
@@ -31,5 +32,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.2.0")]
-[assembly: AssemblyFileVersion("0.11.2.0")]
+[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyFileVersion("0.12.0.0")]
---
+++ RedFoxMQ/NodeGreetingMessage.cs
@@ -0,0 +1,54 @@
+﻿// 
+// Copyright 2013 Hans Wolff
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+using System;
+
+namespace RedFoxMQ
+{
+    class NodeGreetingMessage
+    {
+        private const byte ProtocolVersion = 1;
+
+        public NodeType NodeType { get; private set; }
+
+        public NodeGreetingMessage(NodeType nodeType)
+        {
+            NodeType = nodeType;
+        }
+
+        public static NodeGreetingMessage Deserialize(byte[] buffer)
+        {
+            if (buffer == null) throw new ArgumentNullException("buffer");
+            if (buffer.Length < 2) 
+                throw new RedFoxProtocolException(String.Format("Protocol header (excluding size) must have at least 2 bytes payload but had only {0} bytes", buffer.Length));
+
+            if (buffer[0] != ProtocolVersion)
+                throw new RedFoxProtocolException(String.Format("Protocol version {0} is not supported (expected {1})", buffer[0], ProtocolVersion));
+
+            var message = new NodeGreetingMessage((NodeType)buffer[1]);
+            return message;
+        }
+
+        public byte[] Serialize()
+        {
+            var result = new byte[3];
+            result[0] = (byte)(result.Length - 1);
+            result[1] = ProtocolVersion;
+            result[2] = (byte) NodeType;
+            return result;
+        }
+    }
+}
