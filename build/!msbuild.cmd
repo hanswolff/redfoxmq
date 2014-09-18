1b5271ae7a9e7402f70a9945716501aab781c693
Hans Wolff <hans.wolff.home@gmail.com>
2014-09-18 18:28:02 +0200

    specify release mode in build script

--- build/!msbuild.cmd
+++ build/!msbuild.cmd
@@ -2,5 +2,5 @@
 
 set path=%path%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 
-msbuild /m "..\RedFoxMQ.sln" /p:VisualStudioVersion=12.0 || (pause && exit /b 1)
-msbuild /m "..\RedFoxMQ (.NET 4.5.1).sln" /p:VisualStudioVersion=12.0 || (pause && exit /b 1)
+msbuild /m "..\RedFoxMQ.sln" /p:VisualStudioVersion=12.0 /p:Configuration=Release || (pause && exit /b 1)
+msbuild /m "..\RedFoxMQ (.NET 4.5.1).sln" /p:VisualStudioVersion=12.0 /p:Configuration=Release || (pause && exit /b 1)
